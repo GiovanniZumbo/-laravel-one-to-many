e3677ebfb97f88573991e1b1d0304d26405d3b23
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-11-06 18:20:19 +0100

    models relation explication

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,10 @@ class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["title", 'description', 'image_url', 'git_url'];
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    protected $fillable = ["title", "types_id", 'description', 'image_url', 'git_url'];
 }
